7f4ece4920700f82fcdc39165ab9765cc9731bc2
Carolina Reis <carolvitheusmobile@gmail.com>
2020-09-04 21:16:46 -0300

    cards portfolio

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,32 +6,33 @@ import { Link } from 'react-router-dom'
 
 class App extends Component {
   render() {
-  return (
-    <div style={{height: '660px', position: 'relative'}}>
+    return (
+      <div className="demo-big-content" style={{height: '660px', position: 'relative'}}>
     <Layout style={{background: 'url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover'}}>
-        <Header transparent title="PORTFOLIO" style={{color: 'white'}}>
-            <Navigation>
-              <Link to="/resume">Resumo</Link>
-              <Link to="/aboutme">Sobre Mim</Link>
-              <Link to="projects">Projetos</Link>
-              <Link to="/contact">Contato</Link>
-            </Navigation>
-        </Header>
-        <Drawer title="Menu">
-            <Navigation>
-              <Link to="/resume">Resumo</Link>
-              <Link to="/aboutme">Sobre Mim</Link>
-              <Link to="projects">Projetos</Link>
-              <Link to="/contact">Contato</Link>
-            </Navigation>
-        </Drawer>
-        <Content>
-            <div className="page-content" />
-            <Main/>
-        </Content>
+      <Header transparent title="PORTFOLIO" style={{color: 'white'}}>
+        <Navigation>
+          <Link to="/resume">Resumo</Link>
+          <Link to="/aboutme">Sobre Mim</Link>
+          <Link to="projects">Projetos</Link>
+          <Link to="/contact">Contato</Link>
+        </Navigation>
+      </Header>
+      <Drawer title="Menu">
+        <Navigation>
+          <Link to="/resume">Resumo</Link>
+          <Link to="/aboutme">Sobre Mim</Link>
+          <Link to="projects">Projetos</Link>
+          <Link to="/contact">Contato</Link>
+        </Navigation>
+      </Drawer>
+      <Content>
+        <div className="page-content" />
+          <Main/>
+      </Content>
     </Layout>
-</div>
-  );
-}}
+  </div>
+    );
+  }
+}
 
 export default App;
--- my-app/src/components/projects.js
+++ my-app/src/components/projects.js
@@ -1,22 +1,51 @@
-import React, {Component} from 'react';
-import { Tabs } from 'react-mdl';
+import React, { Component } from 'react';
+import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton } from 'react-mdl';
+
 
 class Projects extends Component {
-    constructor(props) {
-        super(props);
-        this.states = { activeTab: 0};
+  constructor(props) {
+    super(props);
+    this.state = { activeTab: 0 };
+  }
+
+  toggleCategories() {
+
+    if(this.state.activeTab === 0){
+      return(
+        <div className="projects-grid">
+          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'}} >React Project #1</CardTitle>
+            <CardText>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+            </CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>CodePen</Button>
+              <Button colored>Live Demo</Button>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card> 
+        </div>
+    )
+        }
     }
 
     render() {
         return(
             <div className="category-tabs">
             <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
-                <Tab>UI & UX</Tab>
+                <Tab>UI/UX</Tab>
                 <Tab>HTML/CSS/JavaScript</Tab>
                 <Tab>React</Tab>
             </Tabs>
-            <section>
-                <div className="content">Content for the tab: {this.state.activeTab}</div>
+            <section className="projects-grid">
+                <Grid className="projects-grid">
+                    <Cell col={12}>
+                    <div className="content">{this.toggleCategories()}</div>
+                    </Cell>
+                </Grid>
             </section>
         </div>
         )
